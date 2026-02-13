32f4ec11302c68bc331c5d194ee5f7410a90ffdd
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2026-02-12 18:43:28 -0600

    ei

--- src/app.js
+++ src/app.js
@@ -15,24 +15,6 @@ app.use(express.json());
 //Rutas Web.
 app.use("/api/web", require("./routes/web/admin.routes"));
 
-//auth's
-app.use("/api/web/auth", require("./routes/web/authRouter"));
-app.use("/api/movil/auth", require("./routes/movil/authRoute"));
-
-// routers de la web
-app.use("/api/web/estudiantes", require("./routes/web/estudiantesRoute"));
-app.use("/api/web/docentes", require("./routes/web/docenteRoute"));
-app.use("/api/web/materias", require("./routes/web/materiasRoute"));
-app.use("/api/web/grupos", require("./routes/web/grupoRoute"));
-app.use("/api/web/clases", require("./routes/web/claseRoute"));
-app.use("/api/web/asistencias", require("./routes/web/asistenciaRoute"));
-app.use("/api/web/incidencias", require("./routes/web/reporteRoute"));
-app.use("/api/web/accesos", require("./routes/web/accesosRoute"));
-app.use("/api/web/periodos", require("./routes/web/periodosRouter"));
-app.use("/api/web/especialidades", require("./routes/web/especialidadRoute"));
-app.use("/api/web/admins", require("./routes/web/adminsRoutes"));
-app.use("/api/web/bitacoras", require("./routes/web/bitacorasRouter"));
-
 // routers de la movil.
 app.use("/api/movil", require("./routes/movil/app.routes"));
 
