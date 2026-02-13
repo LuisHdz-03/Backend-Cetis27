e2f1735d8bbb4662b71cdd3cf644ab60c5763be3
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2026-02-12 18:21:35 -0600

    si

--- src/routes/movil/estudianteMRoute.js
+++ src/routes/movil/estudianteMRoute.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const router = Router();
+const { verificarToken } = require("../../middlewares/authMiddleware");
 
 const {
   getAlumnosMovil,
 } = require("../../controller/movil/estuudianteMoController");
 
-router.get("/id", getAlumnosMovil);
+router.get("/perfil", verificarToken, getAlumnosMovil);
 
 module.exports = router;
