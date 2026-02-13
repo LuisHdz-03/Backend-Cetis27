9a0b35c4685c3748e07ca2e003af5275df55d88b
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2026-02-12 18:37:54 -0600

    si

--- src/routes/movil/app.routes.js
+++ src/routes/movil/app.routes.js
@@ -1,8 +1,19 @@
 const { Router } = require("express");
 const router = Router();
 
+const { verificarToken } = require("../../middlewares/authMiddleware");
+const { login } = require("../../controller/auth/authController");
+const {
+  getAlumnosMovil,
+} = require("../../controller/movil/estudianteMoController");
+
 router.get("/", (req, res) => {
   res.json({ mensaje: "Bienvenido a la App Móvil (Estudiantes/Docentes)" });
 });
+
+router.post("/auth/login", login);
+router.use(verificarToken);
+
+router.get("/perfil", getAlumnosMovil);
 
 module.exports = router;
--- src/routes/web/admin.routes.js
+++ src/routes/web/admin.routes.js
@@ -1,8 +1,28 @@
 const { Router } = require("express");
 const router = Router();
 
+// Ruta base de prueba (GET /api/web)
 router.get("/", (req, res) => {
   res.json({ mensaje: "Bienvenido al Dashboard Administrativo (Web)" });
 });
+
+// Autenticación
+router.use("/auth", require("./authRouter"));
+
+// Gestión Académica
+router.use("/estudiantes", require("./estudiantesRoute"));
+router.use("/docentes", require("./docenteRoute"));
+router.use("/administrativos", require("./adminsRoutes")); // OJO: Checa si en el front usas "admins" o "administrativos"
+router.use("/grupos", require("./grupoRoute"));
+router.use("/materias", require("./materiasRoute"));
+router.use("/clases", require("./claseRoute"));
+router.use("/periodos", require("./periodosRouter"));
+router.use("/especialidades", require("./especialidadRoute"));
+
+// Gestión Diaria
+router.use("/asistencias", require("./asistenciaRoute"));
+router.use("/incidencias", require("./reporteRoute"));
+router.use("/accesos", require("./accesosRoute"));
+router.use("/bitacoras", require("./bitacorasRouter"));
 
 module.exports = router;
