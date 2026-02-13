f03a99f543e06bba73569354c57c25ce2def961d
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2026-02-12 18:51:29 -0600

    creo ya esta listo

--- src/app.js
+++ src/app.js
@@ -5,17 +5,13 @@ const helmet = require("helmet");
 
 const app = express();
 
-const { verificarToken } = require("./middlewares/authMiddleware");
-
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-//Rutas Web.
 app.use("/api/web", require("./routes/web/admin.routes"));
 
-// routers de la movil.
 app.use("/api/movil", require("./routes/movil/app.routes"));
 
 app.get("/", (req, res) => {
--- src/routes/movil/authRoute.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-const { login } = require("../../controller/auth/authController");
-
-router.post("/login", login);
-
-module.exports = router;
