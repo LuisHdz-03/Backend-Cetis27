b8331bc224ea3f137ec42097c9207e8eb0430546
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2026-03-25 21:51:17 -0600

    cambio de 15 a 3 minutos para volver a iniciar sesion

--- src/middlewares/rateLimitMiddleware.js
+++ src/middlewares/rateLimitMiddleware.js
@@ -1,7 +1,7 @@
 const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 2 * 60 * 1000,
   max: 3,
   standardHeaders: true,
   legacyHeaders: false,
